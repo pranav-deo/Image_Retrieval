68f83e732572a41c4a1b75d9c23d940795c86eca
Pranav Deo <pranavdeo99@gmail.com>
2020-04-28 14:53:15 +0530

    added plain ae, utils for hash viz

---
+++ model/ae.py
@@ -0,0 +1,92 @@
+import torch
+import torch.nn as nn
+
+Num_channels = 8
+inner_channels = 128
+
+
+class AE(nn.Module):
+    """AE for mulitask hashing, reconstruction"""
+
+    def __init__(self):
+        super(AE, self).__init__()
+
+        # Encoder:
+        self.e_conv = nn.ModuleList([])
+        self.e_conv.append(self.give_conv(3, 64))
+        self.e_conv.append(self.give_conv(64, inner_channels))
+        self.e_conv.append(self.give_conv(inner_channels, Num_channels), last=True)
+
+        self.e_block = nn.ModuleList([self.res_block(inner_channels, inner_channels) for _ in range(7)])
+
+        # Decoder
+        self.d_conv = nn.ModuleList([])
+        self.d_conv.append(self.give_conv(Num_channels, inner_channels, T=True))
+        self.d_conv.append(self.give_conv(inner_channels, 64, T=True))
+        self.d_conv.append(self.give_conv(64, 3, T=True, last=True))
+
+        self.d_block = nn.ModuleList([self.res_block(inner_channels, inner_channels) for _ in range(7)])
+
+    def encoder(self, x):
+        conv = self.e_conv
+        block = self.e_block
+
+        c0 = conv[0](x)
+        c1 = conv[1](c0)
+        b0 = block[0](c1)
+        b1 = block[1](b0 + c1)
+        b2 = block[2](b0 + b1)
+        sum1 = b2 + c1 + b1
+        b3 = block[3](sum1)
+        b4 = block[4](b3 + sum1)
+        b5 = block[5](b3 + b4)
+        sum2 = b5 + sum1 + b4
+        b6 = block[6](sum2)
+        sum3 = b6 + c1 + sum2
+        c2 = conv[2](sum3)
+        return torch.sigmoid(c2)
+
+    def decoder(self, x):
+        conv = self.d_conv
+        block = self.d_block
+
+        c0 = conv[0](x)
+        b0 = block[0](c0)
+        b1 = block[1](b0 + c0)
+        b2 = block[2](b0 + b1)
+        sum1 = b2 + c0 + b1
+        b3 = block[3](sum1)
+        b4 = block[4](b3 + sum1)
+        b5 = block[5](b3 + b4)
+        sum2 = b5 + sum1 + b4
+        b6 = block[6](sum2)
+        sum3 = b6 + c0 + sum2
+        c1 = conv[1](sum3)
+        c2 = conv[2](c1)
+        return c2
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return encoded, decoded
+
+    def give_conv(self, in_c, out_c, T=False, last=False):
+        if T:
+            conv = nn.ConvTranspose2d(in_channels=in_c, out_channels=out_c, kernel_size=(5, 5), stride=(2, 2), padding=2, output_padding=1),
+        else:
+            conv = nn.Conv2d(in_channels=in_c, out_channels=out_c, kernel_size=(5, 5), stride=(2, 2), padding=2, output_padding=1),
+
+        net = nn.Sequential(conv, nn.BatchNorm2d(out_c), nn.ReLU()) if last is False else nn.Sequential(conv, nn.BatchNorm2d(out_c))
+        return net
+
+    def res_block(self, in_channels, out_channels, kernel_size=(3, 3), stride=(1, 1)):
+        net = nn.Sequential(
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride),
+            nn.BatchNorm2d(out_channels)
+        )
+        return net
---
+++ utils/hash_utils.py
@@ -0,0 +1,119 @@
+import torch
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.metrics import confusion_matrix
+import pylab as pl
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import matplotlib.patheffects as PathEffects
+import seaborn as sns
+import scipy.spatial as S
+
+import sys
+np.set_printoptions(threshold=sys.maxsize)
+
+
+def get_tensor_paths():
+    path_latent = ["./h/h_{}.pt".format(ii) for ii in range(449)]
+    path_labels = ["./l/l_{}.pt".format(ii) for ii in range(449)]
+    return path_latent, path_labels
+
+
+def load_tensors():
+    path_latent, path_labels = get_tensor_paths()
+    latent_ = [torch.load(path_latent[ii], map_location={'cuda:1': 'cuda:0'}) for ii in range(449)]
+    labels_ = [torch.load(path_labels[ii], map_location={'cuda:1': 'cuda:0'}) for ii in range(449)]
+    latent = torch.cat(latent_).cpu().detach().numpy()
+    labels = torch.cat(labels_).cpu().detach().numpy()
+    return latent, labels
+
+
+def scatter(x, colors):
+    palette = np.array(sns.color_palette("hls", 9))
+    f = plt.figure(figsize=(48, 48))
+    ax = plt.subplot(aspect='equal')
+    sc = ax.scatter(x[:, 0], x[:, 1], lw=0, s=100, c=palette[colors.astype(np.int)])
+    ax.axis('on')
+    ax.axis('tight')
+
+    txts = []
+    for i in range(9):
+        xtext, ytext = np.median(x[colors == i, :], axis=0)
+        txt = ax.text(xtext, ytext, str(i), fontsize=64)
+        txt.set_path_effects([
+            PathEffects.Stroke(linewidth=5, foreground="w"),
+            PathEffects.Normal()])
+        txts.append(txt)
+
+    return f, ax, sc, txts
+
+
+def plot_tsne(latent_binary, kmeans_labels):
+    tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
+    tsne_results = tsne.fit_transform(latent_binary)
+    sns.palplot(np.array(sns.color_palette("hls", 9)))
+    scatter(tsne_results, kmeans_labels)
+    plt.show()
+
+
+def save_array_as_text(array, name, precision):
+    np.savetxt('{}.out'.format(name), array, delimiter=',', fmt='%.{}f'.format(precision))
+
+
+def get_kmeans(array):
+    kmeans = KMeans(n_clusters=9, random_state=0).fit(array)
+    return kmeans
+
+
+def plot_confusion_matrix(y, x):
+    cm = confusion_matrix(y, x)
+    pl.title('Confusion matrix')
+    sns.heatmap(cm, annot=True)
+    plt.xlabel('K means clusters')
+    plt.ylabel('True classes')
+    pl.show()
+
+
+def plot_hamming_distance(latent, labels, num_vec, show_dist=False):
+    rand_vectors = np.random.randint(low=0, high=latent.shape[0], size=num_vec)
+    lat = latent[rand_vectors, :]
+    lab = labels[rand_vectors]
+    ind = np.argsort(lab)
+    lat = lat[ind, :]
+    lab = lab[ind]
+    dis_mat = np.zeros((num_vec, num_vec))
+    for ii in range(lat.shape[0]):
+        for jj in range(ii, lat.shape[0]):
+            dis_mat[ii, jj] = S.distance.hamming(lat[ii, :], lat[jj, :])
+
+    dis_mat += np.transpose(dis_mat)
+    plt.xticks(np.arange(num_vec), lab)
+    plt.yticks(np.arange(num_vec), lab)
+    ax = plt.gca()
+    if show_dist:
+        for (j, i), label in np.ndenumerate(dis_mat):
+            ax.text(i, j, label, ha='center', va='center')
+    plt.title("Hamming distance between random instances")
+    plt.xlabel("Real labels")
+    plt.xlabel("Real labels")
+    plt.imshow(dis_mat, cmap='viridis')
+    plt.colorbar()
+    plt.show()
+
+
+def main():
+    latent, labels = load_tensors()
+    latent_binary = np.sign(latent)
+    # save_array_as_text(latent, "latent", 2)
+    # save_array_as_text(latent_binary, "hash", 0)
+
+    assert np.all((latent_binary == 1) + (latent_binary == -1))
+
+    # kmeans = get_kmeans(latent_binary)
+    # plot_tsne(latent_binary, kmeans.labels_)
+    # plot_confusion_matrix(labels, kmeans.labels_)
+    plot_hamming_distance(latent_binary, labels, 100)
+
+
+if __name__ == '__main__':
+    main()
